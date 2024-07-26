3dea8439f4914a92cf108fb6290951cdc6b91958
Daniel Afriyie <dafriyie7@gmail.com>
2024-07-26 07:47:56 +0000

    adjustments

--- config/dropbox.js
+++ config/dropbox.js
@@ -1,10 +1,11 @@
-const fetch = require('isomorphic-fetch');
+const got = require('got');
+// const fetch = require('isomorphic-fetch');
 const Dropbox = require('dropbox').Dropbox;
 
 // Initialize Dropbox with the access token and fetch function
 const dbx = new Dropbox({
   accessToken: process.env.DROPBOX_ACCESS_TOKEN,
-  fetch
+  got
 });
 
 module.exports = dbx;
