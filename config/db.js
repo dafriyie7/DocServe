fa3d530e1880992316da35149c4449124f89a065
Daniel Afriyie <dafriyie7@gmail.com>
2024-07-26 13:06:56 +0000

    adjustments

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose'); // Import mongoose
+const mongoose = require('mongoose');
 
 // Connect to MongoDB
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL); // Connection string from environment variables
-        console.log(`MongoDB Connected: ${conn.connection.host}`); // Success message
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`MongoDB Connection Error: ${error.message}`); // Error message
+        console.error(`MongoDB Connection Error: ${error.message}`);
         process.exit(1); // Exit on failure
     }
 };
